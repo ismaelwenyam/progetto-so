8fddd580f31e53abc0f6dc790bf184f6a272ef4c
Ismael Wenyam Arnaud <arnaudismael@hotmail.com>
2024-12-12 17:12:58 +0100

    operatore init

--- src/operatore.c
+++ src/operatore.c
@@ -6,14 +6,39 @@
 #include "simerr.h"
 #include "ticket.h"
 #include "semapi.h"
+#include "msgapi.h"
 #include "config_sim.h"
 #include "sim_stats.h"
 
-
-int main (int argc, char **argv){
-	ConfigurationAdt configuration = get_config();
+void draft () {
+        ConfigurationAdt configuration = get_config();
 	printf("operatore.%d.configuration.nof_pause: %d\n", getpid(), configuration.nofPause);
 	// compete to find desk
+
+}
+
+int main (int argc, char **argv){
+	log_time();
+	printf("operatore.pid.%d\n", getpid());	
+	log_time();
+	printf("operatore.pid.%d.init initialization\n", getpid());
+	int msgQueueId;
+	if ((msgQueueId = msgget(PROCESS_TO_DIRECTOR_MSG_KEY, 0)) == -1){
+		log_time();
+		printf("operatore.%d.msgget.ipcr_creat.failed!\n", getpid());
+	}
+	sleep(7);
+	MsgAdt msg;
+	msg.mtype = getpid();
+	strcpy(msg.piAdt.mtext, READY);
+	if (msgsnd(msgQueueId, &msg, sizeof(msg) - sizeof(long), 0) == -1){
+		printf("operatore.pid.%d.msgsnd.failed!\n", getpid());
+		err_exit(strerror(errno));
+	}
+	pause();
+	log_time();
+	printf("operatore.pid.%d.started\n", getpid());	
+	
 	
 	
 
