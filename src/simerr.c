26a9f21f2297f8f05a5fd89ebf218c1696dbe60b
Ismael Wenyam Arnaud <arnaudismael@hotmail.com>
2024-12-12 17:13:52 +0100

    removed log func

--- src/simerr.c
+++ src/simerr.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "simerr.h"
 
-
 void err_exit (char *err){
 	fprintf(stderr, "error occured: %s (%d)\n", err == 0 ? strerror(errno) : err, errno);
 	exit(EXIT_FAILURE);
